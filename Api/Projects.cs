29a816f8b69f89076fb6c18c700388451a251e8c
Fredrik Elestedt <fredrik.elestedt@volvocars.com>
2015-09-15 08:13:20 +0200

    Listing projects.

--- Api/Projects.cs
+++ Api/Projects.cs
@@ -1,14 +1,26 @@
-﻿using SonarQubeApiCSharp.Workers;
+﻿using RestSharp;
+using SonarQubeApiCSharp.Entities;
+using SonarQubeApiCSharp.Workers;
+using System.Collections.Generic;
 
 namespace SonarQubeApiCSharp.Api
 {
     public class Projects
     {
+        private static string RESOURCES = "/api/resources";
+
         private ImprovedRestClient _client;
 
         internal Projects(ImprovedRestClient client)
         {
             _client = client;
+        }
+
+        public List<Project> Get()
+        {
+            var request = new RestRequest(RESOURCES, Method.GET);
+            List<Project> response = _client.Execute<List<Project>>(request);
+            return response;
         }
     }
 }
---
+++ Entities/Project.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SonarQubeApiCSharp.Entities
+{
+    public class Project
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("creationDate")]
+        public DateTime CreationDate { get; set; }
+
+        [JsonProperty("lname")]
+        public string LName { get; set; }
+
+        [JsonProperty("lang")]
+        public string Language { get; set; }
+
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
